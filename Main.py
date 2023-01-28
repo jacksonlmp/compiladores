edc8895d5bef0fb4ae6e892f4b393118a1c88966
Thiago <tcavalcanti.tc@gmail.com>
2023-01-28 12:23:10 -0300

    Criando arquivo para testes e realizando análise léxica

--- Main.py
+++ Main.py
@@ -1,8 +1,21 @@
-import Lexica as lex
+import sys
+sys.path.insert(1, './Análise léxica')
 
+import AnalisadorLexico as lex
+import pandas as pd
 
-lexema = ">="
-print(lex.operadoresLogicos(lexema))
+# Programa a ser analisado
+codigo = open('CodigoDeExemplo.txt', 'r')
 
-lexema = "+"
-print(lex.operadoresAritmeticosAtribuicao(lexema))
+tabela_tokens = lex.realizarAnaliseLexica(codigo)
+
+# Guardando resultado da analise em um arquivo
+arquivo = open("SaídaLéxico.txt", 'w')
+str = tabela_tokens.to_string()
+arquivo.write(str)
+
+# Imprimindo toda a tabela no console de execucao
+with pd.option_context('display.max_rows', None,
+                       'display.max_columns', None,
+                       'display.precision', 3,
+                       ): print(tabela_tokens)
