88c0f3c73c61fcaaaeb40cef089ddba6212c4074
Thiago <tcavalcanti.tc@gmail.com>
2023-04-08 17:17:47 -0300

    Criando estrutura para geracao de codigo de 3 enderecos

---
+++ CodigoIntermediario/TresEnderecos.py
@@ -0,0 +1,12 @@
+import sys
+import pandas as pd
+
+def gerarCodigo3Enderecos(tabelaDeTokens, tabelaDeSimbolos):
+    arquivo = open("Codigo3Enderecos.txt", 'a')
+    str = 'Work in progress' + '\n'
+    
+    # Expressoes de atribuicoes de variaveis
+    # Expressoes dentro de if e while
+    
+    arquivo.write(str)
+    arquivo.close()
--- Main.py
+++ Main.py
@@ -2,9 +2,11 @@ import sys
 sys.path.insert(1, './AnaliseLexica')
 sys.path.insert(1, './AnaliseSintatica')
 sys.path.insert(1, './AnaliseSemantica')
+sys.path.insert(1, './CodigoIntermediario')
 import AnalisadorLexico as lex
 import AnalisadorSintatico as sint
 import AnalisadorSemantico as semantico
+import TresEnderecos as tradutorCodigo3End
 import pandas as pd
 from tabulate import tabulate
 
@@ -24,5 +26,7 @@ tabelaDeSimbolos = sint.realizarAnaliseSintatica(tabelaDeTokens)
 semantico.realizarAnaliseSemantica(tabelaDeTokens, tabelaDeSimbolos)
 
 print(tabulate(tabelaDeSimbolos, headers='keys', tablefmt='pretty'))
+
+tradutorCodigo3End.gerarCodigo3Enderecos(tabelaDeTokens, tabelaDeSimbolos)
 
 print("Codigo compilado com sucesso!")
