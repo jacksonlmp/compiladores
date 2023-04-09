3fc9b854d5de2bd90c237242521583762528ef9b
Thiago <tcavalcanti.tc@gmail.com>
2023-04-09 00:38:04 -0300

    Melhorando impressao dos dados

--- Main.py
+++ Main.py
@@ -10,6 +10,28 @@ import TresEnderecos as tradutorCodigo3End
 import pandas as pd
 from tabulate import tabulate
 
+def imprimirTabelaDeTokens(tabelaDeTokens):
+    print('+-----+--------------------+----------------+-------+')
+    print('|                 Tabela de tokens                  |')
+    print
+    with pd.option_context('display.max_rows', None,
+                            'display.max_columns', None,
+                        'display.precision', 3,
+                        ): print(tabulate(tabelaDeTokens, headers='keys', tablefmt='pretty'))
+
+
+def imprimirTabelaSimbolos(tabelaDeSimbolos):
+    print('\n+----+----------------+----------------+---------+-------+------------------+---------------+--------------+----------------+----------------+')
+    print('|                                                            Tabela de simbolos                                                              |')
+    print(tabulate(tabelaDeSimbolos, headers='keys', tablefmt='pretty'))
+
+
+def imprimirCodigoIntermediario(tabelaDeTokens, tabelaDeSimbolos):
+    print('\n+-----+--------------------+----------------+-------+')
+    print('|              Codigo de 3 enderecos                |')
+    tradutorCodigo3End.gerarCodigo3Enderecos(tabelaDeTokens, tabelaDeSimbolos)
+    print('+-----+--------------------+----------------+-------+\n')
+
 # Programa a ser analisado
 codigo = open('CodigoDeExemplo.txt', 'r')
 
@@ -17,16 +39,12 @@ tabelaDeTokens = lex.realizarAnaliseLexica(codigo)
 
 codigo.close()
 
-with pd.option_context('display.max_rows', None,
-                        'display.max_columns', None,
-                       'display.precision', 3,
-                       ): print(tabulate(tabelaDeTokens, headers='keys', tablefmt='pretty'))
+imprimirTabelaDeTokens(tabelaDeTokens)
 
 tabelaDeSimbolos = sint.realizarAnaliseSintatica(tabelaDeTokens)
 semantico.realizarAnaliseSemantica(tabelaDeTokens, tabelaDeSimbolos)
 
-print(tabulate(tabelaDeSimbolos, headers='keys', tablefmt='pretty'))
-
-tradutorCodigo3End.gerarCodigo3Enderecos(tabelaDeTokens, tabelaDeSimbolos)
+imprimirTabelaSimbolos(tabelaDeSimbolos)
+imprimirCodigoIntermediario(tabelaDeTokens, tabelaDeSimbolos)
 
 print("Codigo compilado com sucesso!")
